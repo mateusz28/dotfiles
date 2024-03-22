4ca7f1f61406ac5b9e16e5fc0bc63cd445bec26b
Mateusz <mateusz28@gmail.com>
2024-03-22 09:59:21 +0100

    Added copilot, fixed dot repeating

---
+++ lua/azajas/plugins/CopilotChat.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    config = function()
+      require("CopilotChat").setup({})
+      -- Show help actions with telescope
+      vim.keymap.set("n", "<leader>ch", function()
+        local actions = require("CopilotChat.actions")
+        require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+      end, { desc = "CopilotChat - Help actions" })
+      -- Show prompts actions with fzf-lua
+      vim.keymap.set("n", "<leader>cp", function()
+        local actions = require("CopilotChat.actions")
+        require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+      end, { desc = "CopilotChat - Prompt actions" })
+    end,
+  },
+}
