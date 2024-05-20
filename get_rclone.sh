e09a18c9d98c1b837cb11ec6c1f9e325056bded6
Mateusz <mateusz28@gmail.com>
2024-05-20 22:55:46 +0200

    Notes using only git

--- get_rclone.sh
+++
@@ -1,7 +0,0 @@
-curl https://rclone.org/install.sh > install.sh
-chmod +x install.sh
-sudo ./install.sh
-rm install.sh
-echo "Paste config file"
-mkdir -p $HOME/.config/rclone/
-nvim $HOME/.config/rclone/rclone.conf
