33a34b1b8f517422a3c2ff1a5880392d72f7c011
Mateusz <mateusz28@gmail.com>
2024-11-13 12:31:21 +0100

    Added open notes keymap, copilot toggle improved

--- lua/azajas/core/custom.lua
+++ lua/azajas/core/custom.lua
@@ -5,17 +5,17 @@ local keymap = vim.keymap
 
 local function adnote(note)
 	local script_path = vim.fn.expand("$HOME/dotfiles/bin/vnote")
-  local tags = function()
-      local status, tag = pcall(function()
-          return vim.fn.input({prompt = "# Note: "})
-      end)
+	local tags = function()
+		local status, tag = pcall(function()
+			return vim.fn.input({ prompt = "# Note: " })
+		end)
 
-      if status then
-          return (tag and tag ~= "") and tag or ""
-      else
-          return "Empty"
-      end
-  end
+		if status then
+			return (tag and tag ~= "") and tag or ""
+		else
+			return "Empty"
+		end
+	end
 	local tags_str = tags()
 	local type = vim.bo.filetype
 	vim.fn.system({
@@ -30,7 +30,10 @@ local function get_lines(count)
 	local start = vim.api.nvim_win_get_cursor(0)[1]
 	local stop = start + count - 1
 	local lines = vim.fn.getline(start, stop)
-  return table.concat(lines, "\n")
+	if type(lines) == "string" then
+		lines = { lines }
+	end
+	return table.concat(lines, "\n")
 end
 
 -- Expose the function to the Vim scripting environment
@@ -38,6 +41,29 @@ _G.get_lines = get_lines
 _G.adnote = adnote
 
 -- Map the '<leader>an' keys to get the current line or a range of lines
-keymap.set({"n", "v"}, "gan", function()
+keymap.set({ "n", "v" }, "gan", function()
 	_G.adnote(_G.get_lines(vim.v.count1))
 end, { noremap = true, desc = "Add a note with the current line" })
+
+-- Open current note
+local function open_notes()
+	-- Get the hostname
+	local handle = io.popen("hostname")
+	if not handle then
+		return
+	end
+	local hostname = handle:read("*a"):gsub("%s+", "")
+	handle:close()
+
+	-- Construct the filename
+	local notes_name = os.date("notes_%Y-%m-%d_" .. hostname .. ".md")
+
+	-- Change directory to $HOME/notes
+	vim.cmd("lcd " .. os.getenv("HOME") .. "/notes")
+
+	-- Open the file in a vertical split
+	vim.cmd("vsplit " .. notes_name)
+end
+
+-- Map the function to a key combination
+vim.keymap.set("n", "<leader>tc", open_notes, { silent = true, noremap = true, desc = "Open notes" })
--- lua/azajas/core/keymaps.lua
+++ lua/azajas/core/keymaps.lua
@@ -11,14 +11,17 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 keymap.set("n", "<leader>tb", function()
-  if vim.o.background == "dark" then
-    vim.o.background = "light"
-  else
-    vim.o.background = "dark"
-  end
+	if vim.o.background == "dark" then
+		vim.o.background = "light"
+	else
+		vim.o.background = "dark"
+	end
+	print("Background: " .. tostring(vim.o.background))
 end, { desc = "Toggle background theme" })
 vim.keymap.set("n", "<leader>td", function()
-	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-end, { silent = true, noremap = true })
+	local diagnostics_enabled = not vim.diagnostic.is_enabled()
+	vim.diagnostic.enable(diagnostics_enabled)
+	print("Diagnostics enabled: " .. tostring(diagnostics_enabled))
+end, { silent = true, noremap = true, desc = "Toggle diagnosticts" })
 
 keymap.set("c", "<C-a>", "<Home>", { desc = "Control a returns to home in cmd" }) --  move current buffer to new tab
